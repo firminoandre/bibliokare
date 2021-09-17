6752c6a35140196dc42aeee561fdeae823d9a14b
Firmino Andre <andrezfirmino@gmail.com>
2021-09-17 15:34:07 -0300

    Navbar do app

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ export const Header = () => {
         
 <div className="rounded-b-lg w-full flex flex-row items-center p-2 justify-between bg-white shadow-xs bg-gray-100">
     <div className="font-extrabold ml-8 text-lg text-gray-800 hidden md:flex">
-        BIBLIOKARE
+        BIBLIOKARE - 
         <svg class="ml-3 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"></path></svg>
     </div>
     <span className="w-full md:w-1/3 h-10 cursor-pointer border border-gray-300 text-sm rounded-full flex">
