1eac40a62fef43dde110a4f393f6e66c2a0028f5
Firmino Andre <andrezfirmino@gmail.com>
2021-09-21 12:02:08 -0300

    config

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express')
+const { resolve } = require('path')
 const app = express()
 
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+
+app.use('/', express.static(
+    resolve(
+        __dirname,  
+        '/.build'
+    ),
+   )
+ )
 
 app.listen(process.env.PORT || 3000, (err) => {
     if (err) {
